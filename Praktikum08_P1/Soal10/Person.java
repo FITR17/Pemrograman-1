d1473101985cb5908ae1629451827a0cdca628f2
unknown <User_38@station-35.(none)>
2012-11-27 10:48:39 +0700

    praktikum8

---
+++ Praktikum08_P1/Soal10/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+	public static void main (String[] args ) {
+		int a=4,b=6;
+		int num=11; 
+		if (num > 5)
+			System.out.println("Number is Greater Than 5");
+
+		else 
+			System.out.println("Number is less Than 5");
+		
+		
+	
+	}
+}
+
